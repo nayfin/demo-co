0b550e3f8d4f265c38af24ae986a6893d285ccd8
nayfin <natjkern@gmail.com>
2021-01-31 23:18:37 -0600

    generates editable lib

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  projects: ['<rootDir>/apps/demo', '<rootDir>/apps/api'],
+  projects: [
+    '<rootDir>/apps/demo',
+    '<rootDir>/apps/api',
+    '<rootDir>/libs/editable',
+  ],
 };
---
+++ libs/editable/README.md
@@ -0,0 +1,7 @@
+# editable
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test editable` to execute the unit tests.
