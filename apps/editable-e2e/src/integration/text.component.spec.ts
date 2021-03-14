ddff329d79f692351bf718050926bcac3d2afc30
nayfin <natjkern@gmail.com>
2021-03-14 15:57:54 -0500

    adds cypress test for background-color

--- apps/editable-e2e/src/integration/text.component.spec.ts
+++ apps/editable-e2e/src/integration/text.component.spec.ts
@@ -1,7 +1,8 @@
 describe('editable', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=textcomponent--primary&knob-backgroundColor&knob-uiState=editing&knob-textValue'));
+  beforeEach(() => cy.visit('/iframe.html?id=textcomponent--primary&knob-backgroundColor&knob-uiState=displaying&knob-textValue'));
 
-  it('should render the component', () => {
-    cy.get('editable-text').should('exist');
+  it('should render the component with correct background-color', () => {
+    cy.get('editable-text').should('exist')
+      .should('have.css', 'background-color', 'rgb(208, 176, 218)');
   });
 });
