40a5fa6a41f5f847f4500901a43b951468150f07
nayfin <natjkern@gmail.com>
2021-04-11 21:30:33 -0500

    adds cypress test

--- apps/editable-e2e/src/integration/text.component.spec.ts
+++ apps/editable-e2e/src/integration/text.component.spec.ts
@@ -1,7 +1,7 @@
 describe('editable', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=textcomponent--primary&knob-backgroundColor&knob-dataStatus=saved&knob-textValue'));
+  beforeEach(() => cy.visit('/iframe.html?id=textcomponent--primary&knob-backgroundColor&knob-dataStatus=saved&knob-textValue=hello'));
 
   it('should render the component', () => {
-    cy.get('editable-text').should('exist');
+    cy.get('editable-text').should('exist').should('contain', 'hello');
   });
 });
